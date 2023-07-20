0b07a26a657e9bf724e4ab86d5079d83b06f7331
harivmasoor <harimasoor@gmail.com>
2023-07-20 10:14:36 -0700

    push

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,10 +1,11 @@
 const cors = require('cors');
 
 const corsOptions = {
-    origin: ['https://harivmasoor.github.io/Podify/', 'https://podify-backend.vercel.app/'],
+    origin: '*',  // Consider specifying the exact domains in a production environment
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 };
 
 module.exports = cors(corsOptions);
+
